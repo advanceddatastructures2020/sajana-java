3c07139f2503e9dfc1380002e4d4eae4cc3c429f
sajanaazeez24 <76766957+sajanaazeez24@users.noreply.github.com>
2021-05-11 17:20:36 +0530

    Add files via upload

---
+++ simplepublic.java
@@ -0,0 +1,17 @@
+class A
+{
+public int data=40;
+public void msg()
+{
+System.out.println("Hello java");
+}
+}
+public class simplepublic
+{
+public static void main(String args[])
+{
+A obj=new A();
+System.out.println(obj.data);
+obj.msg();
+}
+}
